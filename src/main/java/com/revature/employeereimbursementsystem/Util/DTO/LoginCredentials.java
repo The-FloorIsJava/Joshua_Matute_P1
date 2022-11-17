be2ec7d35242ca5e599d0249f242f1affc2485b6
j-matute <josh.matute@gmail.com>
2022-11-17 15:33:03 -0500

    I've created the LoginCredentials class in a new package called DTO(Data Transfer Object). This will handle the login credentials for the employee. I still need to figure out how to make it handle a managers login credentials.

---
+++ src/main/java/com/revature/employeereimbursementsystem/Util/DTO/LoginCredentials.java
@@ -0,0 +1,27 @@
+package com.revature.employeereimbursementsystem.Util.DTO;
+
+// this is the data transfer object that is handling the users login credentials
+
+
+public class LoginCredentials {
+
+        private int employeeID;
+        private String password;
+
+        public int employeeID() {
+            return employeeID;
+        }
+
+        public void setEmployeeID(int employeeID) {
+            this.employeeID = employeeID;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+}
