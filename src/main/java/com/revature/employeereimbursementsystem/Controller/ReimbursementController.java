2f75685f9a881e032f17f50101f7bd60487ace2f
j-matute <josh.matute@gmail.com>
2022-11-21 13:22:33 -0500

    Created the ReimbursementModel.

---
+++ src/main/java/com/revature/employeereimbursementsystem/Controller/ReimbursementController.java
@@ -0,0 +1,17 @@
+package com.revature.employeereimbursementsystem.Controller;
+
+import com.revature.employeereimbursementsystem.Model.Employee;
+import com.revature.employeereimbursementsystem.Service.EmployeeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+
+
+public class ReimbursementController {
+
+    ReimbursementController reimbursementController;
+
+
+}
